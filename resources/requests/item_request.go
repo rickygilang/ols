bdd578a2221e97d09556b282ab25b86fe350ca37
Ricky Gilang Aryadi <42887061+rickygilang@users.noreply.github.com>
2022-04-05 13:16:26 +0700

    Update item_request.go

--- resources/requests/item_request.go
+++ resources/requests/item_request.go
@@ -1,12 +1,16 @@
 package resources_requests
 
+import "ols/models"
+
 type CreateItemRequest struct {
-	ItemName string `json:"name" binding:"required"`
+	ItemName string       `json:"name" binding:"required"`
+	Taxes    []models.Tax `json:"taxes,omitempty"`
 }
 
 type ItemUpdateRequest struct {
-	ItemId   int    `json:"id" binding:"required"`
-	ItemName string `json:"name" binding:"required"`
+	ItemId   int          `json:"id" binding:"required"`
+	ItemName string       `json:"name" binding:"required"`
+	Taxes    []models.Tax `json:"taxes,omitempty"`
 }
 
 type ItemDeleteRequest struct {
